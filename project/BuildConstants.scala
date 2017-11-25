1ae2803623bf4a85ac71f63752cac0bbc9cd814a
Robert Ladstätter <rladstaetter@gmail.com>
2017-11-25 15:41:49 +0100

    updates homework fetching framework and simplifies build

--- project/BuildConstants.scala
+++ project/BuildConstants.scala
@@ -1,5 +1,12 @@
+import sbt._
+
+object Dependencies {
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.3"
+  lazy val jgit = "org.eclipse.jgit" % "org.eclipse.jgit" % "4.9.0.201710071750-r"
+}
+
 object BuildConstants {
   val org: String = "at.fh-joanneum.ima"
-  val buildVer = "2017.11.7-SNAPSHOT"
+  val buildVer = "2017.0.4-SNAPSHOT"
   val scalaVer = "2.12.3"
 }
